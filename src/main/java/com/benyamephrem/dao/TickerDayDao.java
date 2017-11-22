988cd645b2c27a98d7a6be1636c30a10bbd5e173
Benyam Ephrem <ephremdsg@gmail.com>
2017-11-22 09:00:56 -0500

    Added projection fields to mongo queries to ensure only the field we want are returned.

--- src/main/java/com/benyamephrem/dao/TickerDayDao.java
+++ src/main/java/com/benyamephrem/dao/TickerDayDao.java
@@ -11,10 +11,12 @@ public interface TickerDayDao extends MongoRepository<TickerDay, String>{
 
     List<TickerDay> findAll();
 
-    @Query("{ 'date' : ?0 }")
+    //Projection fields specify the only fields we want returned, here we don't want id either since
+    //we have no relationship to traverse between documents
+    @Query("{ 'date' : ?0 }, { date: 1, open: 1, close: 1, _id: 0 }")
     List<TickerDay> findByDate(Date date); //Stored as ISO date aka "yyyy-mm-dd" ----> ISO date before passed into here
 
     //$gte means >= and $lte means <=
-    @Query("{ 'date' : {$gte: ?0, $lte: ?1} }")
+    @Query("{ 'date' : {$gte: ?0, $lte: ?1} }, { date: 1, open: 1, close: 1, _id: 0 }")
     List<TickerDay> findByDateRange(Date start, Date end); //Again remember that what you pass in here must be parsed to ISO
 }
